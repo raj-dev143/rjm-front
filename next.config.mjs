d7e714e83518b38c19c5db91cf82361188601482
raj-dev143 <rajendra.meena.dev@gmail.com>
2024-02-17 15:10:43 +0530

    rjm

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         source: "/api/:path*",
         // destination: "http://localhost:5000/api/:path*", // Change the port to your backend server's port
-        destination: "https://rjm-front.vercel.app/api/:path*", // Change the port to your backend server's port
+        destination: "https://rjm.vercel.app/api/:path*", // Change the port to your backend server's port
       },
     ];
   },
